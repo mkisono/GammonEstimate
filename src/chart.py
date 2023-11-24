ca779981346e852b49316197fd134552187e81de
Masahiro Kisono <mkisono@gmail.com>
2023-11-24 19:11:44 +0900

    refactoring

--- src/chart.py
+++ src/chart.py
@@ -8,103 +8,57 @@ font = dict(
 
 config = {'displayModeBar': False}
 
+
+def _add_trace(fig, x_axis, y_axis, name, color):
+    fig.add_trace(go.Bar(
+        x=[x_axis],
+        y=[y_axis],
+        name=name,
+        orientation='h',
+        marker=dict(
+            color=color
+        ),
+        hoverinfo='skip'
+    ))
+
+
+def _add_annotation(fig, x_axis, y_axis, text):
+    fig.add_annotation(
+        x=x_axis,
+        y=y_axis,
+        text=text,
+        showarrow=False,
+        font=font
+    )
+
+
 def draw_chart(x_axis, y_axis, row):
     fig = go.Figure()
     # Player BG
-    fig.add_trace(go.Bar(
-        y=[y_axis],
-        x=[row[x_axis][5]],
-        name='player_bg',
-        orientation='h',
-        marker=dict(
-            color="#308FB8"
-        ),
-        hoverinfo='skip'
-    ))
+    _add_trace(fig, row[x_axis][5], y_axis, 'player_bg', '#308FB8')
     # Player G
-    fig.add_trace(go.Bar(
-        y=[y_axis],
-        x=[row[x_axis][4] - row[x_axis][5]],
-        name='player_g',
-        orientation='h',
-        marker=dict(
-            color="#5fb2d5"
-        ),
-        hoverinfo='skip'
-    ))
-    fig.add_annotation(
-        x=row[x_axis][5] + (row[x_axis][4] - row[x_axis][5]) / 2,
-        y=0,
-        text=f"{row[x_axis][4]*100:.2f}%",
-        showarrow=False,
-        font=font
-    )
+    _add_trace(fig, row[x_axis][4] - row[x_axis]
+               [5], y_axis, 'player_g', '#5fb2d5')
+    _add_annotation(fig, row[x_axis][5] + (row[x_axis][4] -
+                    row[x_axis][5]) / 2, 0, f"{row[x_axis][4]*100:.2f}%")
     # Player Win
-    fig.add_trace(go.Bar(
-        y=[y_axis],
-        x=[row[x_axis][3] - row[x_axis][4]],
-        name='player_win',
-        orientation='h',
-        marker=dict(
-            color="#9ccfe5"
-        ),
-        hoverinfo='skip'
-    ))
-    fig.add_annotation(
-        x=row[x_axis][4] + (row[x_axis][3] - row[x_axis][4]) / 2,
-        y=0,
-        text=f"{row[x_axis][3]*100:.2f}%",
-        showarrow=False,
-        font=font
-    )
+    _add_trace(fig, row[x_axis][3] - row[x_axis]
+               [4], y_axis, 'player_win', '#9ccfe5')
+    _add_annotation(fig, row[x_axis][4] + (row[x_axis][3] -
+                    row[x_axis][4]) / 2, 0, f"{row[x_axis][3]*100:.2f}%")
     # Opponent Win
-    fig.add_trace(go.Bar(
-        y=[y_axis],
-        x=[row[x_axis][2] - row[x_axis][1]],
-        name='opponent_win',
-        orientation='h',
-        marker=dict(
-            color="#d7eadc"
-        ),
-        hoverinfo='skip'
-    ))
-    fig.add_annotation(
-        x=row[x_axis][3] + (row[x_axis][2] - row[x_axis][1]) / 2,
-        y=0,
-        text=f"{row[x_axis][2]*100:.2f}%",
-        showarrow=False,
-        font=font
-    )
+    _add_trace(fig, row[x_axis][2] - row[x_axis][1],
+               y_axis, 'opponent_win', '#d7eadc')
+    _add_annotation(fig, row[x_axis][3] + (row[x_axis][2] -
+                    row[x_axis][1]) / 2, 0, f"{row[x_axis][2]*100:.2f}%")
     # Opponent G
-    fig.add_trace(go.Bar(
-        y=[y_axis],
-        x=[row[x_axis][1] - row[x_axis][0]],
-        name='opponent_g',
-        orientation='h',
-        marker=dict(
-            color="#a4d1b0"
-        ),
-        hoverinfo='skip'
-    ))
-    fig.add_annotation(
-        x=row[x_axis][3] + (row[x_axis][2] - row[x_axis][1]) +
-        (row[x_axis][1] - row[x_axis][0]) / 2,
-        y=0,
-        text=f"{row[x_axis][1]*100:.2f}%",
-        showarrow=False,
-        font=font
-    )
+    _add_trace(fig, row[x_axis][1] - row[x_axis]
+               [0], y_axis, 'opponent_g', '#a4d1b0')
+    _add_annotation(fig, row[x_axis][3] + (row[x_axis][2] - row[x_axis][1]) + (
+        row[x_axis][1] - row[x_axis][0]) / 2, 0, f"{row[x_axis][1]*100:.2f}%")
     # Opponent BG
-    fig.add_trace(go.Bar(
-        y=[y_axis],
-        x=[row[x_axis][0]],
-        name='opponent_bg',
-        orientation='h',
-        marker=dict(
-            color="#71B784"
-        ),
-        hoverinfo='skip'
-    ))
+    _add_trace(fig, row[x_axis][0], y_axis, 'opponent_bg', '#71B784')
+
     fig.update_xaxes(visible=False, range=[0, 1])
     fig.update_yaxes(visible=False)
     fig.update_layout(barmode='stack', height=100, margin=dict(
@@ -115,77 +69,22 @@ def draw_chart(x_axis, y_axis, row):
 def draw_estimate_chart(player_win, player_g, opponent_g):
     fig = go.Figure()
     # Player G
-    fig.add_trace(go.Bar(
-        y=[0],
-        x=[player_g],
-        name='player_g',
-        orientation='h',
-        marker=dict(
-            color="#5fb2d5"
-        ),
-        hoverinfo='skip'
-    ))
-    fig.add_annotation(
-        x=player_g / 2,
-        y=0,
-        text=f"{player_g}%",
-        showarrow=False,
-        font=font
-    )
+    _add_trace(fig, player_g, 0, 'player_g', '#5fb2d5')
+    _add_annotation(fig, player_g / 2, 0, f"{player_g}%")
     # Player Win
-    fig.add_trace(go.Bar(
-        y=[0],
-        x=[player_win - player_g],
-        name='player_win',
-        orientation='h',
-        marker=dict(
-            color="#9ccfe5"
-        ),
-        hoverinfo='skip'
-    ))
-    fig.add_annotation(
-        x=player_g + (player_win - player_g) / 2,
-        y=0,
-        text=f"{player_win}%",
-        showarrow=False,
-        font=font
-    )
+    _add_trace(fig, player_win - player_g, 0, 'player_win', '#9ccfe5')
+    _add_annotation(fig, player_g + (player_win - player_g) /
+                    2, 0, f"{player_win}%")
     # Opponent Win
-    fig.add_trace(go.Bar(
-        y=[0],
-        x=[100 - player_win - opponent_g],
-        name='opponent_win',
-        orientation='h',
-        marker=dict(
-            color="#d7eadc"
-        ),
-        hoverinfo='skip'
-    ))
-    fig.add_annotation(
-        x=player_win + (100 - player_win - opponent_g) / 2,
-        y=0,
-        text=f"{100 - player_win}%",
-        showarrow=False,
-        font=font
-    )
+    _add_trace(fig, 100 - player_win - opponent_g,
+               0, 'opponent_win', '#d7eadc')
+    _add_annotation(fig, player_win + (100 - player_win -
+                    opponent_g) / 2, 0, f"{100 - player_win}%")
     # Opponent G
-    fig.add_trace(go.Bar(
-        y=[0],
-        x=[opponent_g],
-        name='opponent_g',
-        orientation='h',
-        marker=dict(
-            color="#a4d1b0"
-        ),
-        hoverinfo='skip'
-    ))
-    fig.add_annotation(
-        x=player_win + (100 - player_win - opponent_g) + opponent_g / 2,
-        y=0,
-        text=f"{opponent_g}%",
-        showarrow=False,
-        font=font
-    )
+    _add_trace(fig, opponent_g, 0, 'opponent_g', '#a4d1b0')
+    _add_annotation(fig, player_win + (100 - player_win -
+                    opponent_g) + opponent_g / 2, 0, f"{opponent_g}%")
+
     fig.update_xaxes(visible=False, range=[0, 100])
     fig.update_yaxes(visible=False)
     fig.update_layout(barmode='stack', height=100, margin=dict(
